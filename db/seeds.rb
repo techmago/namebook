8bb3a2db8878c0646c1e36b6b7f6d5c862328f38
Luis Alexandre Deschamps Brandão <techmago@ymail.com>
2013-05-13 14:22:26 -0300

    Adicionados valores default para a tabela raças.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8 
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+race_list = [
+  [ "Humano", "Humanos são uma raça normal." ],
+  [ "Newkies", "Uma raça criada artificialmente. Pelos humanos." ],
+  [ "Lobisomem", "Raça originaria de Hollus capaz de se transformar na forma de um lobo." ]
+]
+
+race_list.each do |name, description|
+  Race.create( nome: name, descr: description )
+end
+  
