269c808cf1b45c1227d4cfc49af45d6f9e877c5c
Luis Alexandre Deschamps Brandão <techmago@ymail.com>
2013-05-17 11:50:49 -0300

    criada a migração para a join table (book,orb)

--- db/migrate/20130517143716_create_books_orbs_join.rb
+++ db/migrate/20130517143716_create_books_orbs_join.rb
@@ -1,7 +1,12 @@
 class CreateBooksOrbsJoin < ActiveRecord::Migration
   def up
+    create_table 'books_orbs', :id => false do |t|
+      t.column 'book_id', :integer
+      t.column 'orb_id', :integer
+    end
   end
 
   def down
+    drop_table 'books_orbs'
   end
 end
