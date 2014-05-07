1998ad35a5f8de028b06fa5e805e518d75c391a4
Luis Alexandre Deschamps Brandão <techmago@ymail.com>
2014-05-07 10:28:20 -0300

    Adicionado clausula down as migrações

--- db/migrate/20130926165750_add_orbit_id_to_orbs.rb
+++ db/migrate/20130926165750_add_orbit_id_to_orbs.rb
@@ -2,4 +2,10 @@ class AddOrbitIdToOrbs < ActiveRecord::Migration
   def change
     add_column :orbs, :orbit_id, :string
   end
+  
+  def down
+    change_table :orbs do |t|
+      t.remove :orbit_id
+    end
+  end
 end
--- db/migrate/20140221185942_add_sexo_to_names.rb
+++ db/migrate/20140221185942_add_sexo_to_names.rb
@@ -2,4 +2,10 @@ class AddSexoToNames < ActiveRecord::Migration
   def change
     add_column :names, :sexo, :integer, :default => 1
   end
+  
+  def down
+    change_table :names do |t|
+      t.remove :sexo
+    end
+  end
 end
