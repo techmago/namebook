ce96482ba0dcb9943a6b09ac9bb42a5bc13d1ddb
Luis Alexandre Deschamps Brandão <techmago@ymail.com>
2014-08-28 15:10:55 -0300

    Atualizado ships_sizes para o padrão do Rails4

--- app/controllers/ship_sizes_controller.rb
+++ app/controllers/ship_sizes_controller.rb
@@ -1,36 +1,22 @@
 #encoding: utf-8
 class ShipSizesController < ApplicationController
+  before_action :set_ship_size, only: [:show, :edit, :update, :destroy]
   # GET /ship_sizes
   # GET /ship_sizes.json
   def index
     @ship_sizes = ShipSize.order('nome')
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @ship_sizes }
-    end
   end
 
   # GET /ship_sizes/1
   # GET /ship_sizes/1.json
   def show
     @ship_size = ShipSize.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @ship_size }
-    end
   end
 
   # GET /ship_sizes/new
   # GET /ship_sizes/new.json
   def new
     @ship_size = ShipSize.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @ship_size }
-    end
   end
 
   # GET /ship_sizes/1/edit
@@ -41,7 +27,7 @@ class ShipSizesController < ApplicationController
   # POST /ship_sizes
   # POST /ship_sizes.json
   def create
-    @ship_size = ShipSize.new(params[:ship_size])
+    @ship_size = ShipSize.new(ship_size_params)
 
     respond_to do |format|
       if @ship_size.save
@@ -57,10 +43,8 @@ class ShipSizesController < ApplicationController
   # PUT /ship_sizes/1
   # PUT /ship_sizes/1.json
   def update
-    @ship_size = ShipSize.find(params[:id])
-
     respond_to do |format|
-      if @ship_size.update_attributes(params[:ship_size])
+      if @ship_size.update_attributes(ship_size_params)
         format.html { redirect_to @ship_size, notice: 'O tamanho de nave foi atualizado com sucesso.' }
         format.json { head :no_content }
       else
@@ -73,7 +57,6 @@ class ShipSizesController < ApplicationController
   # DELETE /ship_sizes/1
   # DELETE /ship_sizes/1.json
   def destroy
-    @ship_size = ShipSize.find(params[:id])
     @ship_size.destroy
 
     respond_to do |format|
@@ -81,4 +64,16 @@ class ShipSizesController < ApplicationController
       format.json { head :no_content }
     end
   end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ship_size
+      @ship_size = ShipSize.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def ship_size_params
+      params.require(:ship_size).permit(:nome, :descr)
+    end
 end
+
+
